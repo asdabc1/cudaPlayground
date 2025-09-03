5738f24b2e19e386b6af2f6568becc5dfc11aed4
Adam Bożek <adambozek12@gmail.com>
2025-09-03 04:12:34 +0200

    Kogge-Stone adder implementation

--- main.cu
+++ main.cu
@@ -1,68 +1,55 @@
 #include <iostream>
 
-__global__ void suma(float* input, float* output, int size) {
-    const int COARSE_FACTOR = 2;
-    const int blockdim = 128;
-    __shared__ float temp[blockdim];
+const int threads = 256;
+__global__ void kogge_stone(float* input, int size, float* output) {
+    __shared__ float temp[threads];
 
-    int segment_start = COARSE_FACTOR * 2 * blockDim.x * blockIdx.x; // index początku każdego segmentu po coarseningu - tworzymy podział wejścia na segmenty
-    int segment_pos = segment_start + threadIdx.x; //pozycja danego wątku w swoim segmencie
-
-    if (segment_pos < size)
-        temp[threadIdx.x] = input[segment_pos];
+    temp[threadIdx.x] = input[threadIdx.x];
 
     __syncthreads();
 
-    for (int i = 1; i < COARSE_FACTOR * 2; i++) {
-        if (threadIdx.x + (i + blockIdx.x) * blockDim.x < size)
-            temp[threadIdx.x] += input[segment_pos + i * blockDim.x];
-    }
-
-    __syncthreads();
-
-    for (int step = blockDim.x / 2; step > 0; step /= 2) {
-        if (threadIdx.x < step) {
-            temp[threadIdx.x] += temp[threadIdx.x + step];
+    for (int step = 1; step < size; step *= 2) {
+        __syncthreads();
+        if (threadIdx.x >= step) {
+            temp[threadIdx.x] += temp[threadIdx.x - step];
         }
     }
 
+    output[threadIdx.x] = temp[threadIdx.x];
     __syncthreads();
-
-    if (threadIdx.x == 0) {
-        atomicAdd(output, temp[0]);
-    }
 }
 
 int main() {
-    float data[1024];
-    float sum = 0;
+    float dane[256];
+    for (int i = 0; i < 256; i++)
+        dane[i] = 1;
 
-    for (int i = 0; i < 1024; i++) {
-        data[i] = i;
-    }
+    float wynik[256];
 
-    float* numbers, *s;
+    float* a1, *a2;
 
-    cudaMalloc((void**)&numbers, sizeof(float) * 1024);
-    cudaMalloc((void**)&s, sizeof(float) * 1024);
+    cudaMalloc((void**)&a1, threads * sizeof(float));
+    cudaMalloc((void**)&a2, threads * sizeof(float));
 
-    cudaMemset(s, 0, sizeof(float));
+    cudaMemcpy(a1, dane, threads * sizeof(float), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(numbers, data, sizeof(float) * 1024, cudaMemcpyHostToDevice);
-
-    suma<<<2, 128>>>(numbers, s, 1024);
+    kogge_stone<<<1, threads>>>(a1, threads, a2);
 
     cudaDeviceSynchronize();
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
-        std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
+        std::cout << "Error: " << cudaGetLastError() << std::endl;
     }
 
-    cudaMemcpy(&sum, s, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(wynik, a2, sizeof(float) * threads, cudaMemcpyDeviceToHost);
 
-    cudaFree(numbers);
-    cudaFree(s);
+    cudaFree(a1);
+    cudaFree(a2);
 
-    std::cout << "suma to: " << sum << std::endl;
+    for (int i = 0; i < 256; i++) {
+        std::cout << wynik[i] << " ";
+        if ( i % 16 == 0 && i != 0 )
+            std::cout << std::endl;
+    }
     return 0;
 }
